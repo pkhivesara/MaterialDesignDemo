2c16e829930ea52edc7d24f0135fde2d9d1eea52
prats.khive <prats.khive@gmail.com>
2015-11-06 18:28:32 -0800

    create new fragment

--- app/src/main/java/com/test/materialdesigndemo/MainActivity.java
+++ app/src/main/java/com/test/materialdesigndemo/MainActivity.java
@@ -1,16 +1,25 @@
 package com.test.materialdesigndemo;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        frameLayout = (FrameLayout)findViewById(R.id.frame_layout);
         setSupportActionBar(toolbar);
+        FirstFragment firstFragment = new FirstFragment();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.frame_layout, firstFragment);
+        transaction.commit();
     }
 }
