9b8a071139df88ed605ef805b4598b766eddaa6a
pkhivesara <prats.khive@gmail.com>
2016-01-15 11:16:49 -0800

    add service class for direct share

---
+++ app/src/main/java/com/test/materialdesigndemo/ShareService.java
@@ -0,0 +1,36 @@
+package com.test.materialdesigndemo;
+
+import android.content.ComponentName;
+import android.content.IntentFilter;
+import android.graphics.drawable.Icon;
+import android.os.Bundle;
+import android.service.chooser.ChooserTarget;
+import android.service.chooser.ChooserTargetService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 512136 on 1/15/2016.
+ */
+public class ShareService extends ChooserTargetService {
+    @Override
+    public List<ChooserTarget> onGetChooserTargets(ComponentName componentName, IntentFilter intentFilter) {
+        ArrayList<ChooserTarget> resultSet = new ArrayList<ChooserTarget>();
+        for(int i = 0; i<6; i++){
+            resultSet.add(buildNewTarget(i));
+        }
+        return resultSet;
+    }
+
+        private ChooserTarget buildNewTarget(int i){
+        String title = "test";
+            Icon icon = Icon.createWithResource(getApplicationContext(),R.drawable.ic_himym_1);
+        float score = 0.5f;
+        ComponentName componentName1 = new ComponentName(this,MainActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putInt("ID",1);
+        return new ChooserTarget(title,icon,score,componentName1,bundle);
+    }
+    }
+
